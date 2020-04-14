2f174926acaa4cf936ce17357c25eea73628b292
Meghashree541 <63413806+Meghashree541@users.noreply.github.com>
2020-04-14 17:12:28 +0530

    Add files via upload

---
+++ vote.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main() {
+    int age;
+    cout<<"Enter your age:"<<endl;
+    cin>>age;
+    if(age>=18)
+    { 
+        cout<<"You are eligible to vote:"<<endl;
+     }
+    else
+    {
+        cout<<age<<"\nYour age is  less than 18"<<"\nWait until 18"<<endl;
+    }
+    return 0;
+}
